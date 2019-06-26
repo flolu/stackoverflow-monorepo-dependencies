3d7aad630bc3d44a95a691ebfb7607863d4b2c52
skatestyle <fllu@protonmail.com>
2019-06-26 07:55:25 +0200

    ✅ fixed issue through @omarseddik 's proposal

--- packages/common/src/index.ts
+++ packages/common/src/index.ts
@@ -1,3 +1,4 @@
 const magicNumber: number = 4242;
+const message: string = 'its working!';
 
-export { magicNumber };
+export { magicNumber, message };
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,7 +1,11 @@
 import express from 'express';
-import { magicNumber } from '@root/common';
+import { magicNumber, message } from '@root/common';
 const server = express();
 
+server.get('**', (req, res) => {
+  res.send(message);
+});
+
 server.listen(magicNumber, () => {
-    console.log('server listening on port', magicNumber);
+  console.log('server listening on port', magicNumber);
 });
